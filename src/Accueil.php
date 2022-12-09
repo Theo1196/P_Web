47f5cd7a8a090e63e22390a07fe8d2f8f9dabd27
Theo1196 <pb64ois@eduvaud.ch>
2022-12-09 08:45:21 +0100

    HTML page liste recette

--- src/Accueil.php
+++ src/Accueil.php
@@ -27,8 +27,8 @@ Description : Page pour la page d'accueil du site Recettes.
 
         <div class="Container-header">
             <ul>
-                <li><a id="active" class="nav-btn" href=""><h1>Accueil</h1></a></li>
-                <li><a class="nav-btn" href="AddRecette.php"><h1>Ajouter une recette</h1></a></li>
+                <li><a class="nav-btn" href="" style=" text-decoration: underline;"><h1>Accueil</h1></a></li>
+                <li><a class="nav-btn" href="AddRecette.php" ><h1>Ajouter une recette</h1></a></li>
                 <li><a class="nav-btn" href="Recette.php"><h1>Liste des recettes</h1></a></li>
                 <li><a class="nav-btn" href="Contact.php"><h1>Contact</h1></a></li>
             </ul>
