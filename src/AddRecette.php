ede944214decf52fdadfcccdbfb1ac512871e594
Theo1196 <pb64ois@eduvaud.ch>
2022-12-19 10:08:45 +0100

    Improve nav

--- src/AddRecette.php
+++ src/AddRecette.php
@@ -2,7 +2,7 @@
 Auteur : moi 
 Date : 25.11.2022
 Lieu : ETML 
-Description : Page pour la page d'accueil du site Recettes.
+Description : Page pour la page d"accueil du site Recettes.
 -->
 <!DOCTYPE html>
 <html lang="fr">
@@ -27,10 +27,18 @@ Description : Page pour la page d'accueil du site Recettes.
 
         <div class="Container-header">
             <ul>
-                <li><a class="nav-btn" href="Accueil.php" ><h1>Accueil</h1></a></li>
-                <li><a class="nav-btn" href="AddRecette.php" style=" text-decoration: underline;"><h1>Ajouter une recette</h1></a></li>
-                <li><a class="nav-btn" href="ListRecette.php"><h1>Liste des recettes</h1></a></li>
-                <li><a class="nav-btn" href="Contact.php"><h1>Contact</h1></a></li>
+                <li><a class="nav-btn" href="Accueil.php">
+                        <h1>Accueil</h1>
+                    </a></li>
+                <li><a class="nav-btn" href="AddRecette.php" style=" text-decoration: underline;">
+                        <h1>Ajouter une recette</h1>
+                    </a></li>
+                <li><a class="nav-btn" href="ListRecette.php">
+                        <h1>Liste des recettes</h1>
+                    </a></li>
+                <li><a class="nav-btn" href="Contact.php">
+                        <h1>Contact</h1>
+                    </a></li>
 
             </ul>
         </div>
@@ -42,25 +50,82 @@ Description : Page pour la page d'accueil du site Recettes.
         $db = new Database();
         $recttes = $db->getAllRecettes();
         // foreach($recttes as $rectte){
-            
-        //     echo "<img src=". $rectte["recImage"] ." alt='image'>";
+
+        //     echo "<img src=". $rectte["recImage"] ." alt="image'>";
         //     // echo '<img src="data:image/jpeg;base64,'.base64_encode($rectte['recImage']).'"/><br>';
         // }
         // important d'avoir enctype dans le form
-        echo "<form method='post' action='checkRecette.php' enctype='multipart/form-data'>";
-        echo "<label for='recName'>Nom de la recette: </label>";
-        echo "<input type='text' name='recName' id='recName'><br>";
-        echo "<label for='recCategorie'>categorie: </label>";
-        echo "<input type='text' name='recCategorie' id='recCategorie'><br>";
-        echo "<label for='recPreparation'>preparation: </label>";
-        echo "<input type='text' name='recPreparation' id='recPreparation'><br>";
-
-        echo "<input type='file' name='addFile'>";
-        echo "<input type='submit' value='upload'></form>";
-
-
-
         ?>
+        <form method="post" action="checkRecette.php" enctype="multipart/form-data">
+            <div class="col-md-4">
+                <div class="form-group label-floating">
+                    <label class="control-label" for="recName">Nom de la recette: </label>
+                    <input class="form-control" type="text" name="recName" id="recName" v-model="Nom"><br>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="form-group label-floating">
+                    <label class="control-label" for="recCategorie">Categorie: </label>
+                    <input class="form-control" type="text" name="recCategorie" id="recCategorie" v-model="Categorie"><br>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="form-group label-floating">
+                    <label class="control-label" for="recTemps">Temps de préparation/cuisson: </label>
+                    <input class="form-control" type="text" name="recTemps" id="recTemps" v-model="Temps"><br>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="form-group label-floating">
+                    <label class="control-label" for="recIngredient">Ingrédients: </label>
+                    <input class="form-control" type="text" name="recIngredient" id="recIngredient" v-model="Ingrédients"><br>
+                </div>
+            </div>
+            <button type="button" id="clickMe">Ajouter un ingredient</button>
+            <div class="row" id="addRowsHere">
+                
+            </div>
+                <div class="col-md-4">
+                    <div class="form-group label-floating">
+                        <label class="control-label" for="recPreparation">Préparation: </label>
+                        <input class="form-control" type="text" name="recPreparation" id="recPreparation" v-model="Préparation"><br>
+                    </div>
+                </div>
+
+                <input type="file" name="addFile">
+                <input type="submit" value="upload">
+        </form>
+
+        <script>
+            document.getElementById("clickMe").onclick = function() {
+                //first div
+                var newDivCol = document.createElement("div");
+                newDivCol.setAttribute("class", "col-md-4");
+                //second div
+                var newDivForm = document.createElement("div");
+                newDivForm.setAttribute("class", "form-group label-floating");
+                newDivCol.appendChild(newDivForm);
+
+                //label
+                var newlabel = document.createElement("label");
+                newlabel.setAttribute("class", "control-label");
+                newlabel.innerHTML = "Nouveau ingrédient";
+                newDivForm.appendChild(newlabel);
+
+                //input
+                var newInput = document.createElement("input");
+                newInput.setAttribute("type", "text");
+                newInput.setAttribute("class", "form-control");
+                newInput.setAttribute("v-model", "Ingrédients");
+                newDivForm.appendChild(newInput);
+
+                var element = document.getElementById("addRowsHere");
+                element.appendChild(newDivCol);
+            };
+        </script>
     </main>
     <footer>
         <hr>
